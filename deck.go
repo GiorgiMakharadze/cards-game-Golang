ad5d788f896a5e72e187fdd1e83d346d55f928ae
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-08-24 20:42:01 +0400

    added functions & writeFile

--- deck.go
+++ deck.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 type deck []string
 
 func newDeck() deck {
@@ -23,3 +26,15 @@ func (d deck) print() {
 		fmt.Println(i,card)
 	}
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")	
+}
+
+func (d deck) saveToFile(filename string ) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}	
