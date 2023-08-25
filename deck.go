003bf56f649acfe6e3c144f8d5357dba73f13acb
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-08-25 07:38:53 -0700

    shuffle

--- deck.go
+++ deck.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
+
 type deck []string
 
 func newDeck() deck {
@@ -38,6 +41,7 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string ) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }	
+
 func newDeckFromFile(filename string) deck {
 	bs, err := os.ReadFile(filename)
 	if err != nil {
@@ -47,4 +51,15 @@ func newDeckFromFile(filename string) deck {
 	s := strings.Split(string(bs), ",")
 
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
